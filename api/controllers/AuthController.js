399d3ed182f67c1aa699818f3ade394a26a9612e
Conner Frey <connerfrey@Conners-MacBook-Pro-2.local>
2016-08-03 17:41:20 -0600

    added salting passwords and tested loggin in and signing up

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -6,6 +6,7 @@
  */
 
  var Promise = require('bluebird');
+var password = require('password-hash-and-salt');
 
  module.exports = {
   session: function(req, res) {
@@ -15,44 +16,71 @@
       return res.json(req.session.user);
     } else {
       console.log("redirect: session isn't set");
-      // return res.redirect('/entrance');
       res.status(400).send('No session');
     }
   },
   login: function(req, res) {
     var data = req.params.all();
-    User.findOne({username: data.username, password: data.password})
+    User.findOne({username: data.username})
+    .populate('items')
+    .populate('subscriptions')
+    .populate('groups')
+    .populate('tags')
+    .populate('chats')
     .then(function(user) {
       if(!user) {
         res.status(400).send('That user was not found!');
       }
 
-      user.password = "";
-      delete user.password;
-      req.session.user = user;
-      console.log("session is set: req.session", req.session);
-      res.json(user);
+      password(data.password).verifyAgainst(user.password, function(error, verified) {
+        if(error)
+          throw new Error('Something went wrong!');
+        if(!verified) {
+          console.log("Don't try! We got you!");
+          res.status(400).send('bad password!');
+        } else {
+          user.password = "";
+          delete user.password;
+          req.session.user = user;
+          console.log("session is set: req.session", req.session);
+          res.json(user);
+        }
+      });
     });
   },
-   signup: function(req, res) {
-     var data = req.params.all();
-     console.log(data);
-     User.create({email: data.email, password: data.password, username: data.username, shopName: data.shopName})
-     .exec(function(err, user) {
-       if(err) {
-         res.status(400).send('That user already exists!');
-       }
-       console.log("signed up user", user);
-       user.password = "";
-       delete user.password;
+  signup: function(req, res) {
+    console.log("signup");
+    var data = req.params.all();
+    // console.log(data);
+    var user = {};
 
-       req.session.user = user;
-       console.log("req.session", req.session);
-       res.json(user);
-     });
-   },
+    password(data.password).hash(function(error, hash) {
+      if(error) {
+        throw new Error('Something went wrong!');
+      }
+
+      // Store hash (incl. algorithm, iterations, and salt)
+      user.password = hash;
+      user.email = data.email;
+      user.username = data.username;
+      user.shopName = data.shopName;
+      User.create(user)
+      .exec(function(err, newUser) {
+        if(err) {
+          res.status(400).send('That user already exists!');
+        }
+        console.log("signed up user", newUser);
+        newUser.password = "";
+        delete newUser.password;
+
+        req.session.newUser = newUser;
+        console.log("req.session", req.session);
+        res.json(newUser);
+      });
+    });
+  },
   logout: function(req, res) {
     delete req.session.user;
     res.ok();
   }
- };
+};
